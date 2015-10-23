c3ef71891b5e66ca294fdd3d3f6652a3a17b165a
Tom MacWright <tom@macwright.org>
2015-10-23 11:51:11 -0400

    Add JSDoc

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ var https = require('https'),
 var base = 'https://s3.amazonaws.com/geojson-please/';
 var naturalEarthSlug = 'naturalearth-3.3.0';
 
+/**
+ * @name geojsonXyz
+ * @param {string} fileSearch query: either a complete filename or a
+ * search term
+ * @param {Function} callback called with (err, { file, geojson })
+ * @returns {undefined} calls callback
+ */
 module.exports = function (fileSearch, callback) {
   var file = fuzzy(fileSearch);
 
   if (!file) return callback('no file matched your query', null);
 
   var url = base + naturalEarthSlug + '/' + file.original;
-  https.get(url, function(res) { 
+  https.get(url, function(res) {
     var body = '';
     res.on('data', function (chunk) {
       body += chunk;
