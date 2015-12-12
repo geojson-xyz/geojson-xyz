9ff04f10a5b89260347a5f5074ff7e4a8bd55ca5
Tom MacWright <tom@macwright.org>
2015-12-12 18:11:50 -0500

    Use geojson-xyz-data

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var https = require('https'),
   fuzzy = require('./lib/fuzzy');
 
-var base = 'https://s3.amazonaws.com/geojson-please/';
-var naturalEarthSlug = 'naturalearth-3.3.0';
-
 /**
  * @name geojsonXyz
  * @param {string} fileSearch query: either a complete filename or a
@@ -16,13 +13,12 @@ module.exports = function (fileSearch, callback) {
 
   if (!file) return callback('no file matched your query', null);
 
-  var url = base + naturalEarthSlug + '/' + file.original;
-  https.get(url, function(res) {
+  https.get(file.original.url, function(res) {
     var body = '';
     res.on('data', function (chunk) {
       body += chunk;
     }).on('end', function () {
-      callback(null, {file: file.original, geojson: JSON.parse(body)});
+      callback(null, {file: file.original.name, geojson: JSON.parse(body)});
     });
   }).on('error', function(err) {
     callback(err, null);
