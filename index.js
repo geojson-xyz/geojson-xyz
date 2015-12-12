0afa602b5fa8b63814b351a34661678da64e9266
Tom MacWright <tom@macwright.org>
2015-12-12 18:32:00 -0500

    Cleanup, bugfixes

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var https = require('https'),
   fuzzy = require('./lib/fuzzy');
 
+function getURL(url, callback) {
+  https.get(url, function(res) {
+    var body = '';
+    res.on('data', function (chunk) {
+      body += chunk;
+    }).on('end', function () {
+      callback(null, JSON.parse(body));
+    });
+  }).on('error', function(err) {
+    callback(err, null);
+  });
+}
+
 /**
  * @name geojsonXyz
  * @param {string} fileSearch query: either a complete filename or a
@@ -8,19 +21,14 @@ var https = require('https'),
  * @param {Function} callback called with (err, { file, geojson })
  * @returns {undefined} calls callback
  */
-module.exports = function (fileSearch, callback) {
+function getGeoJSON(fileSearch, callback) {
   var file = fuzzy(fileSearch);
-
   if (!file) return callback('no file matched your query', null);
-
-  https.get(file.original.url, function(res) {
-    var body = '';
-    res.on('data', function (chunk) {
-      body += chunk;
-    }).on('end', function () {
-      callback(null, {file: file.original.name, geojson: JSON.parse(body)});
-    });
-  }).on('error', function(err) {
-    callback(err, null);
+  getURL(file.original.url, function (err, res) {
+      if (err) throw err;
+      callback(null, { data: res, file: file.original });
   });
 };
+
+module.exports.getGeoJSON = getGeoJSON;
+module.exports.getURL = getURL;
