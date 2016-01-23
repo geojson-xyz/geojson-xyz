bb841a33854a5a0780bbd8699514a9e8243ee5cd
Anand Thakker <vestibule@anandthakker.net>
2016-01-23 13:44:55 -0500

    Add options to get multiple files' data
     - Repurposes unused 'all' option
     - Refactors getGeoJSON to only yield metadata

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 var https = require('https'),
   fuzzy = require('./lib/fuzzy');
 
+/**
+ * @name geojsonXyz
+ * @param {string} fileSearch query: either a complete filename or a
+ * search term
+ * @param {object} options
+ * @param {Function} callback called with (err, [{ file, score, featureMatch }])
+ * @returns {undefined} calls callback
+ */
+function getGeoJSON(search, options, callback) {
+  fuzzy.findMatches(search, function (err, matches) {
+    if (!matches.length) return callback('no file matched your query', null);
+    var topMatch = matches[0];
+    matches = matches.filter(function (match, i) {
+      if (!options.all) { return i === 0; }
+      return match.score === topMatch.score;
+    })
+    .map(function (match) {
+      return {
+        file: match.original,
+        featureMatch: match.value ? {
+          property: match.property,
+          value: match.value
+        } : false,
+        score: match.score
+      };
+    });
+    return callback(null, matches);
+  });
+}
+
 function getURL(url, callback) {
   https.get(url, function(res) {
     var body = '';
@@ -14,22 +44,5 @@ function getURL(url, callback) {
   });
 }
 
-/**
- * @name geojsonXyz
- * @param {string} fileSearch query: either a complete filename or a
- * search term
- * @param {Function} callback called with (err, { file, geojson })
- * @returns {undefined} calls callback
- */
-function getGeoJSON(fileSearch, callback) {
-  fuzzy(fileSearch, function (err, match) {
-    if (!match) return callback('no file matched your query', null);
-    getURL(match.original.url, function (err, res) {
-      if (err) throw err;
-      callback(null, { data: res, file: match.original, match: match });
-    });
-  });
-}
-
-module.exports.getGeoJSON = getGeoJSON;
 module.exports.getURL = getURL;
+module.exports.getGeoJSON = getGeoJSON;
